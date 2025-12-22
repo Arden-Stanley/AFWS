fcc1c497cdc1cedd75288f073b4e8ae66d664b7e
tommypaxton72 <tommypaxton72@gmail.com>
2025-12-21 23:54:22 -0600

    Worked more on the statemachine

--- autonomous_control/AGV/lib/StateMachine/StateMachine.cpp
+++ autonomous_control/AGV/lib/StateMachine/StateMachine.cpp
@@ -1,41 +1,32 @@
-#include <Arduino.h>
 #include "StateMachine.h"
-#include "AGVPinDefs.h"
 #include "LidarC1.h"
 
-//Notes
-// Ransac linefitting for row detection?
-// Maybe do a heatmap for obstacle detection?
-// IMU/GPS Fusion for better localization
-// Kalman Filter for IMU/GPS Fusion
-// Deskewing Lidar Data
-// Do we want to constantly check state?
+// Notes
+// need to find a way to seperate this library from others more
+// Need some type of yaw and pitch compensation
+// Need pose struct that fuses gps with IMU with roll yaw pitch x,y,z with the refernce frames starting at (0,0,0)
+// Expects a MotorControl class, Pose struct
+// Probably need to use a few more pointers
 
-#define agvBubble 254 // Bubble around AGV in mm
+float pi = 3.14159265359;
 
+StateMachine::StateMachine() : mState(MainState::ManualDrive), sState(SubState::START), prevState(SubState::Start)
 
-LidarC1 lidar(Serial1)
-
-
-// Not sure if needed
-void StateMachine::Start() {
-    // (Do Once)
-        // Reset Variables
-        // GetLocalCoordPose
-        // Get pointer to ScanData
-
+void StateMachine::Init() {
+    mState = MainState::ManualDrive;
+    sState = SubState::START;
 
 }
 
 // Main State-Machine Switch
+// Need some type of tracker for state switch
 void StateMachine::Run() {
-    Switch (MainState) {
-        case MainState::AutoDrive:
-            currentState = START;
+    Switch (mState) {
+        case mState::AutoDrive:
             StateMachine::RunAuto();
             break;
 
-        case MainState::ManualDrive:
+        case mState::ManualDrive:
             StateMachine::RunManual();
             break;
 
@@ -48,6 +39,7 @@ void StateMachine::Run() {
 // Manual Control Code
 void StateMachine::RunManual() {
     // Manual Motor Control
+    // Turn this into a different class
     if (agvrx.Output.LWForward > 0 && agvrx.Output.LWReverse == 0) {
       analogWrite(LPWM,agvrx.Output.LWForward);
       digitalWrite(LINA, HIGH);
@@ -89,171 +81,155 @@ void StateMachine::RunManual() {
       agvrx.Output.LWReverse,
       agvrx.Output.RWForward,
       agvrx.Output.RWReverse);
-      delay(250);
     #endif
 
 
 }
 
-// AutoDrive Control Code
-void StateMachine::RunAuto() {
-    StateMachine::StateDetection();
-    switch (CurrentState) {
+void StateMachine::runAuto() {
+    lidar.GetFullScan(2);
+    pose.UpdatePose();
+    StateMachine::CollisionDetection();
+    switch (sState) {
+        case START:
+            StateMachine::Start();
+            break;
         case INBETWEEN_ROWS:
             StateMachine::InbetweenRows();
             break;
         case END_OF_ROW:
             StateMachine::EndofRow();
             break;
+        case TURNING:
+            StateMachine::Turning();
+            break;
+        case ALIGNING:
+            StateMachine::Aligning();
+            break;
         case COLLISION_AVOIDANCE:
             StateMachine::CollisionAvoidance();
-            break;
-        default:
-            // Defaults when no row detected on either side
-            // Might cause an issue with the end of row state moving so far out
-
             break;
     }
 }
 
+void StateMachine::TransitionTo() {
+
+}
+
+void StateMachine::CollisionDetection(){
+    // Should i iterate through a for loop or add this to the lidarc1 library so it sets an interrupt
+    if (m.distance < collisionThreshold) {
+        mControl.Stop();
+        sState = COLLISION_AVOIDANCE;
+    }
+}
+
+void StateMachine::Start() {
+    // Apply brakes
+    // Calibrate sensors
+    // Zero out reference frame
+    // Confirm starting state
+    mControl.Stop();
+
+}
+
 void StateMachine::InbetweenRows() {
-    // Pointer to ScanData
-    // IMU/GPS Fusion
-    // Ransac Line Fit to find row edges
-    // Create waypoints inbetween rows
+    // Confirm left and/or right rows
+    // If no left or right rows switch to end of row
+    // RANSAC
+    // Create waypoint based on ransac
+    // Drive to waypoint
+    uint8_t i;
+    rowPresent();
+    if (!rightDetected && !leftDetected) {
+        sState = END_OF_ROW;
+    }
+    RansacConversion();
+    // RANSAC
+    // find middle line between two ransac lines with respect to field coordinate frame
+    // create waypoint
+    // ransacMidPoint is the offset from the center of AGV to midpoint between two ransac lines
+    waypoint.x[waypoint.i] = pose.x + ransacMidPoint * sin(pose.yawRad);
+    waypoint.y[waypoint.i] = pose.y + set.waypointOffset * cos(pose.yawRad);
+    waypoint.i++;
 
 }
 
 void StateMachine::EndofRow() {
-    // Pointer to ScanData
-    // IMU/GPS Fusion
-    // Find End of Row
-    // Pass End of Row by z amount
-    // Detect which side row is on (use leftRow and rightRow?)
-    // Turn towards next row using constant radius turn with row edge as reference
+    // Mark Compass heading
+    // Move out of row on current heading for z distance
+    // switch to Turning State
+    // How do decide which direction to turn?
+    // find beginning of new row?
+    //
+    if (pose.compass > 180) {
+        float EORmark = pose.compass - 180;
+    }
+    if (pose.compass < 180) {
+        float EORmark = pose.compass + 180;
+    }
+    if ()
+    DetectNextDirection();
 
 }
 
 void StateMachine::Turning() {
-
+    // Make constant radius turn
+    // Wait till 180 deg from previous compass marking
+    // Change to aligning state
+    if (pose.compass <= EORmark + 5 || pose.compass >= EORmark - 5) {
+        sState = ALIGNING;
+    }
 }
 
-void StateMachine:: Alignment() {
+void StateMachine::Aligning() {
+    if (leftDetected || rightDetected) {
+        sState = INBETWEEN_ROWS;
+    }
+    LookForward();
+    // Maybe use old row coord + estimated row offset = new row coords
+    // see if rows left and right then switch to inbetween rows
 
 }
 
 void StateMachine::CollisionAvoidance() {
-    // Pointer to ScanData
-    // Find clear path to reverse to previous waypoint?
-    // Maybe follow previous path backwards
-
+    // Follow previous waypoints in reverse
+    // Switch to previous state?
 }
 
-void StateMachine::StateDetection() {
-    // Pointer to ScanData
-    // IMU/GPS Fusion
-    // Map Building
-    // Sanity Checks
-    // State Transition Logic
-        //Start
-            //
-        //Inbetween Rows
-            //
-        //End of Row
-            //
-
-
-    // TODO: pose Library?
-    Lidar.GetFullScan(1);
-    RowPresent();
-    if (StateMachine::InbetweenRowCondition) {
-        CurrentState = CurrentState::INBETWEEN_ROWS;
-    } else if (StateMachine::EndofRowCondition) {
-        CurrentState = CurrentState::END_OF_ROW;
-    } else if (StateMachine::CollisionAvoidanceCondition) {
-        CurrentState = CurrentState::COLLISION_AVOIDANCE;
-    }
-}
-
-bool StateMachine::InbetweenRowCondition() {
-    // Logic to determine if inbetween rows
-        if (leftRow || rightRow) { // Both Left and Right Detected
-            return true;
+void RansacConversion () {
+    for (uint16_t i; i < m.count; i++) {
+        if (m.angle[i] > 0 && m.angle[i] < 180) {
+            float rad = m.angle[i] * (pi/180);
+            leftrow.x = m.distance[i] * sin(rad);
+            leftrow.y = m.distance[i] * cos(rad);
         }
-        if (LeftRow) {
-            return true;
+        if (m.angle[i] > 180 && m.angle[i] < 360) {
+            float rad = m.angle[i] * (pi/180);
+            leftrow.x = m.distance[i] * sin(rad);
+            leftrow.y = m.distance[i] * cos(rad);
         }
-        return false;
-}
-
-bool StateMachine::EndofRowCondition() {
-    // Logic to determine if at end of row
-    if (!leftRow || !rightRow) {
-        return true;
     }
-    return false;
 }
 
-bool StateMachine::CollisionAvoidanceCondition() {
-    // Logic to determine if collision avoidance is needed
-    // Might be better to do collision detection in lidar library so we dont need to do a for loop and we can check it as we get the data
-    // Using polar lidar data
-    #ifdef DISTANCE
-    uint16_t* mPtr = Measurement.Distance
-    for (uint16_t i = 0; i < sizeof(mPtr); i++) {
-        if (*mptr[i] < agvBubble) {
-            return true;
+void RowPresent() {
+    for (uint16_t i; i < m.count; i++) {
+        // Is this done at compile or runtime?
+        if (m.angle[i] > (90-set.detectionAngle/2) && m.angle < (90+set.detectionAngle/2) && m.distance[i] < set.detecctionThreshold) {
+                uint8_t leftHit++;
         }
-        return false;
-    }
-    #endif
-
-    // TODO: Using rectangular coord data
-    #ifdef RECTANGULAR
-    collisionDistance =
-    if () {
-    return true;
-    }
-    return false;
-    #endif
-
-void StateMachine::ResetALL() {
-    // Reset all variables and states
-}
-
-void StateMachine::ResetManual() {
-    // Reset variables and states after manual intervention
-}
-
-// Might need to break this into functions one for left and one for right
-bool RowPresent() {
-    // leftMin = minimum angle for left side of scanner
-    // leftMax = maximum angle for left side of scanner
-    // rightMin = minimum angle for right side of scanner
-    // rightMax = maximum angle for right side of scanner
-    // threshold = theshold distance for it to be counted as a hit
-    // minHits = minimum number of hits for function to be satisfied that there is a row to the left or right
-    uint8_t leftHit = 0;
-    uint8_t rightHit = 0;
-    for (uint16_t i = 0; i < sizeof(Measurements[0].Angle); i++) {
-        if (Measurements[0].Angle[i] > leftMin && Measurements[0].Angle[i] < leftMax) {
-            if (Measurement[0].Distance[i] < threshold) {
-                leftHit++;
-            }
+        if (m.angle[i] > (90-set.detectionAngle/2) && m.angle < (90+set.detectionAngle/2) && m.distnace[i] < set.detectionThreshold) {
+                uint8_t rightHit++;
         }
-        if (Measurements[0].Angle[i] > rightMin && Measurements[0].Angle[i] < rightMax)
-            if (Measurements[0].Distance[i] < threshold) {
-                rightHit++;
-            }
-    }
-    if (leftHit > minHits) {
-        bool leftRow = true;
+    } // TODO: turn these into ternary operators?
+    if (leftHit > set.hitThreshold) {
+        bool leftDetected = true;
     } else {
-        leftRow = false;
+        leftDetected = false;
     }
-    if (rightHit > minHits) {
-        bool rightRow = true;
+    if (rightHit > set.hitThreshold) {
+        bool rightDetected = true;
     } else {
-        leftRow = false;
+        rightDetected = false;
     }
 }
--- autonomous_control/AGV/lib/StateMachine/StateMachine.h
+++ autonomous_control/AGV/lib/StateMachine/StateMachine.h
@@ -3,21 +3,55 @@
 #include <Arduino.h>
 #include "AGVPinsDef.h"
 
+
+
+
 enum class mainState {
     AutoDrive,
     ManualDrive
 };
 
+// Do we need enum class or enum?
 enum class subState {
+    START,
     INBETWEEN_ROWS,
     END_OF_ROW,
+    TURNING,
+    ALIGNING,
+    ESTOP,
     COLLISION_AVOIDANCE
 };
 
+struct Config {
+    detectionAngle = 180;
+
+
+};
+
+struct pointData {
+    float x,
+    float y
+};
+
+struct line {
+  uint8_t A, B, C
+};
+
+struct Waypoint {
+    float x,
+    float y,
+    uint16_t i
+};
+
+
 class stateMachine {
     public:
-
-
+        MainState mState;
+        SubState sState;
+        SubState prevState;
+        pointData leftRow;
+        pointData rightRow;
+        Config set;
     private:
 
 
