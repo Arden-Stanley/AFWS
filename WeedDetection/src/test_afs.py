40ed85abba640569ab409f3b35bb44a85b457e7c
Justin Finn <jfinn5@ut.utm.edu>
2026-01-14 10:06:23 -0600

    I added pi_camera_active() to be able to open up camera with opencv and the ability to predict objects. It is running off only weed plants so it is hard to see if this works properly.

--- WeedDetection/src/test_afs.py
+++ WeedDetection/src/test_afs.py
@@ -1,3 +1,4 @@
+import cv2
 from ultralytics import YOLO
 import torch
 
@@ -5,9 +6,28 @@ def send_signal():
     print(f"Sending high signal now..")
 
 def pi_camera_active():
+    data = cv2.VideoCapture(0)
+    model = YOLO("runs/detect/train21/weights/best.pt")
+    while data.isOpened:
+        verify, frame = data.read() #verify to call later to validity, frame for 1 frame per 4.5ms
+        if not verify:
+            break # breaks out of script if it can't call back a frame 
+        #print(f"Video capture has been opened successfully")
+        current_results = model(frame, conf =.25) #used to predict with our trained model 
+        single_frame = current_results[0].plot() #plots the data in the terminal 
+
+        cv2.imshow("yolo", single_frame) #Displays the single image in the window 
+
+        k= cv2.waitKey(1) & 0xFF
+        if k == ord('q'): # press q to quit 
+            break
+    
+    data.release()
+    data.destroyAllWindows()
 
 
-def main():
+
+def test():
     img = "img.jpg"
     model = YOLO("runs/detect/train21/weights/best.pt") #loads in our new model for yolov8
     weed_results = model.predict(f"../test/{img}")
@@ -16,6 +36,9 @@ def main():
     print(f"The amount of bounding boxes in this picture: {len(result_example.boxes)}")
     # Run pi camera and get results per image 
     # Get coordinates of bounding box, move target to location and spray 
+
+def main():
+    pi_camera_active()
 
 
 
