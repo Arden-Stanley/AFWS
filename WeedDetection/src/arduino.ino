d6bc7b5939ab35e3e078e104ace0f0561793696b
Justin Finn <122637243+Justinfinn2891@users.noreply.github.com>
2026-01-22 09:00:39 -0800

    Arduino code for our test.

---
+++ WeedDetection/src/arduino.ino
@@ -0,0 +1,27 @@
+bool isTrue = false;
+void setup() {
+ Serial.begin(9600);
+ pinMode(13, OUTPUT);
+ 
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    String verify = Serial.readString();
+    int received_verify = verify.toInt();
+    digitalWrite(13, LOW);
+    
+    if(isTrue == false){
+      isTrue = true;
+      Serial.println("Received");
+      if(received_verify == 0){
+        digitalWrite(13, HIGH);
+        delay(5000);
+        digitalWrite(13, LOW);
+        delay(5000);
+        isTrue = false;
+      }
+    }
+  }
+
+}
