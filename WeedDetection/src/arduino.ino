dc27ee8323c22debcc7360dcf08f33c47f4f7d26
Justin Finn <122637243+Justinfinn2891@users.noreply.github.com>
2026-01-22 09:15:19 -0800

    Updating arduino file, testing some stuff

--- WeedDetection/src/arduino.ino
+++ WeedDetection/src/arduino.ino
@@ -1,3 +1,4 @@
+/*
 bool isTrue = false;
 void setup() {
  Serial.begin(9600);
@@ -25,3 +26,27 @@ void loop() {
   }
 
 }
+
+*/
+// Testing some things 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char verify = Serial.parseInt();   // Reads one character 
+    digitalWrite(13, LOW);         // Turns off the pump 
+
+    if (verify == 0) {           // compare to character '0'
+      digitalWrite(13, HIGH);      // turn on motor
+      delay(5000);
+      digitalWrite(13, LOW);       // turn off motor
+      delay(5000);
+    }
+  }
+}
+
+
