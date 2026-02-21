eda19a6fd510e2df451da100f4cbf7e928c04d7f
Justin Finn <122637243+Justinfinn2891@users.noreply.github.com>
2026-02-20 22:57:57 -0600

    Fixed some mistakes in the code, commented out the serial communication for testing. Worked on the .ino for the teensy and hooked the teensy up to the rasberry pi

--- WeedDetection/src/test_afs.py
+++ WeedDetection/src/test_afs.py
@@ -50,10 +50,127 @@ def save_to_file(x1,y1, name):
         info.write(f"Cell: {name} | X: {x1}, Y:{y1} \n")
 
 
-def arduinoSignal(ser, a):
-    data_to_send = a
-    ser.write(data_to_send.encode('utf-8'))
-    print(f"Sent: {data_to_send.strip()}")
+def arduinoSignal(ser, a):  
+    ser.write(a.encode('utf-8')) 
+    print(f"Sent: {a.strip()}")
+
+# Going to use this class to separate the frame into cells 
+class Grid:
+    def __init__(self,cell_name,x1_axis,x2_axis,y1_axis,y2_axis):
+        self.cell_name = cell_name
+        self.x1_axis = x1_axis
+        self.x2_axis = x2_axis
+        self.y1_axis = y1_axis
+        self.y2_axis = y2_axis
+
+# Testing code to draw out the cells before implementing it
+def test_grid():
+    face_camera = cv2.VideoCapture(0)
+    cell_01 = Grid("cell-01", 0, 160, 300, 480)
+    cell_02 = Grid("cell-02", 161, 320, 300, 480)
+    cell_03 = Grid("cell-03", 321, 480, 300, 480)
+    cell_04 = Grid("cell-04", 481, 640, 300, 480)
+    FONT_SIZE = .5
+    TEXT_COLOR = (255,255,255)
+    THICKNESS = 1
+    while face_camera.isOpened():
+        verify, frame = face_camera.read()
+        cv2.rectangle(frame,(cell_01.x1_axis,cell_01.y1_axis),(cell_01.x2_axis,cell_01.y2_axis),(0,255,0),3)
+        cv2.rectangle(frame,(cell_02.x1_axis,cell_02.y1_axis),(cell_02.x2_axis,cell_02.y2_axis),(255,255,0),3)
+        cv2.rectangle(frame,(cell_03.x1_axis,cell_03.y1_axis),(cell_03.x2_axis,cell_03.y2_axis),(0,255,255),3)
+        cv2.rectangle(frame,(cell_04.x1_axis,cell_04.y1_axis),(cell_04.x2_axis,cell_04.y2_axis),(100,44,320),3)
+
+        cv2.putText(frame, "cell-1", ((cell_01.x2_axis - 55), (cell_01.y1_axis + 20)), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, TEXT_COLOR,THICKNESS)
+        cv2.putText(frame, "cell-2", ((cell_02.x2_axis - 55), (cell_02.y1_axis + 20)), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, TEXT_COLOR,THICKNESS)
+        cv2.putText(frame, "cell-3", ((cell_03.x2_axis - 55), (cell_03.y1_axis + 20)), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, TEXT_COLOR,THICKNESS)
+        cv2.putText(frame, "cell-4", ((cell_04.x2_axis - 55), (cell_04.y1_axis + 20)), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, TEXT_COLOR,THICKNESS)
+        if not verify:
+            break
+        k= cv2.waitKey(1) & 0xFF
+        if k == ord('q'): # press q to quit 
+            break
+        cv2.imshow("yolo", frame)
+    face_camera.release()
+    face_camera.destroyAllWindows()       
+
+def test_face(isDetected):
+
+
+    #TO DO:
+
+... (106 lines left)
+
+test_afs.txt
+9 KB
+/*
+bool isTrue = false;
+void setup() {
+ Serial.begin(9600);
+ pinMode(13, OUTPUT);
+ 
+
+sketch_feb20a.ino
+3 KB
+﻿
+gamerdude
+gamerdude1
+ 
+ 
+import cv2
+from ultralytics import YOLO
+import torch
+import serial
+import time
+
+# TO DO LIST
+# 1.) Allow multiple "solonoids" to go off at a time 
+# 3.) Automate the camera config. ie Extra camera size and divide by total cell count, apply to grid objects. 
+
+def send_signal():
+    print(f"Sending high signal now..")
+
+def pi_camera_active():
+    data = cv2.VideoCapture(0)
+    model = YOLO("runs/detect/train21/weights/best.pt")
+    while data.isOpened:
+        verify, frame = data.read() #verify to call later to validity, frame for 1 frame per 4.5ms
+        if not verify:
+            break # breaks out of script if it can't call back a frame 
+        #print(f"Video capture has been opened successfully")
+        current_results = model(frame, conf =.25) #used to predict with our trained model 
+        single_frame = current_results[0].plot() #plots the data in the terminal 
+
+        cv2.imshow("yolo", single_frame) #Displays the single image in the window 
+    
+        k= cv2.waitKey(1) & 0xFF
+        if k == ord('q'): # press q to quit 
+            break
+    
+    data.release()
+    data.destroyAllWindows()
+
+
+
+def test():
+    img = "img.jpg"
+    model = YOLO("runs/detect/train21/weights/best.pt") #loads in our new model for yolov8
+    weed_results = model.predict(f"../test/{img}")
+    result_example = weed_results[0]
+    weed_results[0].show()
+    print(f"The amount of bounding boxes in this picture: {len(result_example.boxes)}")
+    # Run pi camera and get results per image 
+    # Get coordinates of bounding box, move target to location and spray 
+
+# This will be used to test the face 
+
+def save_to_file(x1,y1, name):
+    with open("test.txt", "a") as info:
+        info.write(f"Cell: {name} | X: {x1}, Y:{y1} \n")
+
+
+def arduinoSignal(ser, a):  
+    ser.write(a.encode('utf-8')) 
+    print(f"Sent: {a.strip()}")
 
 # Going to use this class to separate the frame into cells 
 class Grid:
@@ -116,7 +233,7 @@ def test_face(isDetected):
     face_camera = cv2.VideoCapture(0)
 
     try:
-        ser = serial.Serial('COM10', 9600, timeout = 1)
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
         time.sleep(2)
     except serial.SerialException as e:
         print(f"There was an error opening serial port: {e} ")
@@ -148,7 +265,8 @@ def test_face(isDetected):
         cv2.putText(frame, "cell-3", ((cell_03.x2_axis - 55), (cell_03.y1_axis + 20)), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, TEXT_COLOR,THICKNESS)
         cv2.putText(frame, "cell-4", ((cell_04.x2_axis - 55), (cell_04.y1_axis + 20)), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, TEXT_COLOR,THICKNESS)
 
-        #ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
+        hand_detected = [False, False, False, False]
         # This will take each invidiual frame that has been predicted with face_model and draw bounding boxes around them 
         for result in current_results:
             boxes = result.boxes
@@ -160,7 +278,6 @@ def test_face(isDetected):
                 bounding_x = (x1 + x2) / 2
                 bounding_y = (y1 + y2) / 2
                 #print(f"This is bounding_x: {bounding_x}, this is y: {bounding_y}")
-                hand_detected = [False, False, False, False]
                 grid = [(cell_01, "Cell-01", '0'), (cell_02, "Cell-02", '1'), (cell_03, "Cell-03",'2'), (cell_04, "Cell-04",'3')]
                 for i, (cell, label, signal) in enumerate(grid):
                     grid_system = (
@@ -169,8 +286,8 @@ def test_face(isDetected):
                     if grid_system and not hand_detected[i]:
                         hand_detected[i] = True
                         save_to_file(x1,y1, label)
-                        #arduinoSignal(ser, '3')
-                        print(f"Weed detected at {label}, moving AFS 5 feet forward...")
+                        arduinoSignal(ser, signal)
+                        print(f"Weed detected at {label}, moving AFS 5 feet forward..., and signal {signal}")
                     if not grid_system:
                         hand_detected[i] = False
 
@@ -204,3 +321,5 @@ if __name__ == "__main__":
 
 
 
+test_afs.txt
+9 KB
